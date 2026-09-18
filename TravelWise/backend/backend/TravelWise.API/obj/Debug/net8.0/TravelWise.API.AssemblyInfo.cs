811018b80716dfeb81f4fbed5e61ec7c9db895aa
shehanweersing <shehanweersing@gmail.com>
2026-09-18 10:46:33 +0530

    create the file architecture

--- TravelWise/backend/backend/TravelWise.API/obj/Debug/net8.0/TravelWise.API.AssemblyInfo.cs
+++ TravelWise/backend/backend/TravelWise.API/obj/Debug/net8.0/TravelWise.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelWise.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c6e390bb56d68f6237b9486d2e1a8f440065c86")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelWise.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelWise.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
