6b6f90798e33ae1f9c89538ff97d66dbdf0cdd85
shehanweersing <shehanweersing@gmail.com>
2026-09-21 17:54:53 +0530

    configer new table

---
+++ TravelWise/backend/backend/TravelWise.API/Migrations/20260921122349_AddReadinessTable.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TravelWise.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReadinessTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TravelDocuments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TripId = table.Column<int>(type: "integer", nullable: false),
+                    DocumentType = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    DocumentNumber = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    ExpiryDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsVerified = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TravelDocuments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TravelDocuments_Trips_TripId",
+                        column: x => x.TripId,
+                        principalTable: "Trips",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TravelDocuments_TripId",
+                table: "TravelDocuments",
+                column: "TripId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TravelDocuments");
+        }
+    }
+}
